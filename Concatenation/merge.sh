792045ec853ff0c206c106062e90ed56d66b59f5
Abdelrahman Hosny <abdelrahman.hosny@hotmail.com>
2016-05-02 14:11:20 -0400

    finished merging all patients data

---
+++ Concatenation/merge.sh
@@ -0,0 +1,11 @@
+patient_id=$1
+mkdir Patients/p_DS_bkm_$patient_id
+
+cat KIM_0290_BC6833ANXX/Project_5065_F/Sample_DS-bkm-$patient_id-N/Sample_DS-bkm-$patient_id-N-R2.fastq.gz > Patients/p_DS_bkm_$patient_id/N-R2.fastq.gz
+
+cat KIM_0290_BC6833ANXX/Project_5065_F/Sample_DS-bkm-$patient_id-N/Sample_DS-bkm-$patient_id-N-R1.fastq.gz > Patients/p_DS_bkm_$patient_id/N-R1.fastq.gz
+
+
+cat KIM_0290_BC6833ANXX/Project_5065_F/Sample_DS-bkm-$patient_id-T/Sample_DS-bkm-$patient_id-T-R2.fastq.gz > Patients/p_DS_bkm_$patient_id/T-R2.fastq.gz
+
+cat KIM_0290_BC6833ANXX/Project_5065_F/Sample_DS-bkm-$patient_id-T/Sample_DS-bkm-$patient_id-T-R1.fastq.gz > Patients/p_DS_bkm_$patient_id/T-R1.fastq.gz
