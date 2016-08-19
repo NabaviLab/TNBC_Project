c2a6ad2d3a320ba57e17597e350a18638372dcef
Abdelrahman Hosny <abdelrahman.hosny@hotmail.com>
2016-08-19 09:42:22 -0400

    good morning

---
+++ gene-analysis/Quality/fastqc.sh
@@ -0,0 +1,13 @@
+#!/bin/bash
+#$ -N concatenate
+#$ -M abdelrahman@engr.uconn.edu
+#$ -m n
+#$ -S /bin/bash
+#$ -cwd
+#$ -pe smp 1
+#$ -o fastqc_$JOB_ID.out"
+#$ -e fastqc_$JOB_ID.err"
+
+cd $1
+fastqc R1.fastq.gz
+fastqc R2.fastq.gz
