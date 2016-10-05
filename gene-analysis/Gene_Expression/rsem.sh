c23ff5253ee22d1e61c0506640e292b02e7faae0
Abdelrahman Hosny <abdelrahman.hosny@hotmail.com>
2016-10-05 00:58:13 -0400

    modified script of RSEM for gene expression

--- gene-analysis/Gene_Expression/rsem.sh
+++ gene-analysis/Gene_Expression/rsem.sh
@@ -5,22 +5,33 @@
 #$ -S /bin/bash
 #$ -cwd
 #$ -pe smp 4
-#$ -o rsem_$JOB_ID.out"
-#$ -e rsem_$JOB_ID.err"
+#$ -o $JOB_ID.out"
+#$ -e $JOB_ID.err"
 
 # Uncomment the following lines to prepare the references
 # If you see an error that says "According to the GTF file given, a transcript has exons on multiple chromosomes!"
 # use Ensembl GTF file as UCSC RefSeq annotations allow for some RefSeq mRNA to map to multiple locations on the genome
 
-#rsem-prepare-reference --gtf hg19-annotations-refseq.gtf \
+#rsem-prepare-reference --gtf annotations/Homo_sapiens.GRCh37.75.uncommented.prefixed.gtf \
 #genome.fa \
-#ref/human_gencode
+#human_ensembl
 
 
 # The below lines are to calculate expression values
+# rsem-calculate-expression -p 4 --paired-end \
+# --bam \
+# --estimate-rspd \
+# --output-genome-bam \
+# $1'STAR/star_outputAligned.toTranscriptome.out.bam' \
+# hg19/rsem_ref_new/human_ensembl $1'RSEM2/rsem'
+
+module load STAR/2.5.2a
+
+mkdir $1'RSEM3'
 rsem-calculate-expression -p 4 --paired-end \
---bam \
+--star --star-path /common/opt/bioinformatics/STAR-2.5.2a/bin \
+--gzipped-read-file \
 --estimate-rspd \
 --output-genome-bam \
-$1'star_outputAligned.toTranscriptome.out.bam' \
-hg19/rsem_ref/human_gencode $1'rsem_'
+$1'FASTQ/R1.fastq.gz' $1'FASTQ/R2.fastq.gz' \
+hg19/human_ensembl $1'RSEM3/rsem'
